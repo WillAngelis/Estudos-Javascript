159b35b18b649c5daca5e285dbb651e51aa15156
William Angelis <willangelis@gmail.com>
2021-12-04 18:34:11 -0300

    Conversão de tipos no js

---
+++ 12- convertendo-tipos.js
@@ -0,0 +1,7 @@
+console.log('Conversão de Tipos')
+console.log("2" + "2")// concatena por ser duas strings
+console.log(parseInt('2')+ parseInt('2'))
+console.log('10' / '2')// Na divisão o interpretador transforma as strings em numeros
+console.log('Will'/'2') // Not a Number. Ele não converte e entende como não sendo um número
+
+//parseInt para números inteiros e parseFloat para números com virgula
