2f5fd94ffe83a092cebf99bac9819d464b3869b2
William Angelis <willangelis@gmail.com>
2021-12-04 18:31:48 -0300

    Mudança de nome

--- console-log.js
+++
@@ -1,5 +0,0 @@
-console.log('WillAngelis');
-console.log("WillAngelis");
-console.log(`WillAngelis`); //Strings = Texto
-
-console.log(12345, 14.52)//Mumber = Não há separações de números flutuantes e inteiros todos são do tipo Number
