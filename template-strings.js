859cacf284c6b43b4418bdbea13c8f23f74fa327
William Angelis <willangelis@gmail.com>
2021-12-04 18:32:28 -0300

    Mudança de nome

--- template-strings.js
+++
@@ -1,9 +0,0 @@
-//Template Strings//
-const nome = 'Will';
-const sobreNome = 'Angelis';
-const idade = 25
-let anoNascimento = 2021 - 25
-
-console.log(`${nome} ${sobreNome} tem ${idade} anos`)
-console.log(`${nome} nasceu em ${anoNascimento}`)
-
