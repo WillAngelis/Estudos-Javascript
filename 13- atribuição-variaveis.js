f4c538835ed03ace6680dcfcc0acc6d865b63f08
William Angelis <willangelis@gmail.com>
2021-12-04 18:34:23 -0300

    Atribuindo novos valores as variaveis

---
+++ 13- atribuição-variaveis.js
@@ -0,0 +1,16 @@
+console.log('Atribuição de variaveis')
+
+let nome = 'will'
+const sobrenome = 'angelis'
+const idade = 23
+
+console.log(nome, sobrenome)//
+console.log(nome + '' + sobrenome)
+console.log(`Meu nome é ${nome} e meu sobrenome é ${sobrenome}`)
+
+// Atribuindo valor novo a nome
+// nome = nome + sobrenome
+// console.log(nome)
+
+nomeCompleto = nome + sobrenome
+console.log(nomeCompleto)
