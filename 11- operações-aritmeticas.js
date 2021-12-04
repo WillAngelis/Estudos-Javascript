4e511be7fdc749f21885dede2df07aa4e04f6956
William Angelis <willangelis@gmail.com>
2021-12-04 18:34:04 -0300

    Trabalhando com operações aritmeticas

---
+++ 11- operações-aritmeticas.js
@@ -0,0 +1,6 @@
+console.log('Operações aritméticas');
+
+console.log(2+2)// Soma de dois números
+console.log(2-1)// Subtração de dois números
+console.log(2*3)// Multiplicação de dois números
+console.log(2/3)// Divisão de dois números
