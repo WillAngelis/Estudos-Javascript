f39cba97c7a4ed31e87174a202f05110da41df28
William Angelis <willangelis@gmail.com>
2021-12-06 02:54:13 -0300

    Projeto para estudo de createElement e appendChild

---
+++ 27- imc-project.js
@@ -0,0 +1,32 @@
+const criaDiv = document.querySelector('.btnDiv')
+const criaP = document.querySelector('.btnP')
+const input = document.querySelector('#texto')
+const div = document.createElement('div')
+const p = document.createElement('p')
+const btn = document.createElement('button')
+
+criaP.addEventListener('click',function(e){
+    e.preventDefault()
+    div.appendChild(btn)
+    btn.textContent = 'Ola'
+    return
+})
+criaDiv.addEventListener('click', function(e){
+    e.preventDefault()
+    criarDiv()
+})
+function criarDiv(){
+    document.body.appendChild(div)
+    div.appendChild(p)
+    classeDiv()
+    return 
+}
+
+function classeDiv(){
+    div.classList.add('div__fundo')
+    p.classList.add('p__center')
+    p.textContent = `${input.value}`
+}
+btn.addEventListener('click', function(){
+    div.remove()
+})
