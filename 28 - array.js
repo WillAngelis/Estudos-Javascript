fbd28b7314f743d358ea971590906cf2b654fdd4
William Angelis <willangelis@gmail.com>
2021-12-06 23:53:51 -0300

    Estudo Array

---
+++ 28 - array.js
@@ -0,0 +1,13 @@
+const list_fruits = Array()
+
+list_fruits[0] = 'pera'
+list_fruits[1] = 'maça'
+list_fruits[2] = 'banana'
+console.log(list_fruits)
+
+const list_colors = ['azul','verde']
+console.log(list_colors[0])
+
+//atributo length
+
+console.log(list_colors.length)
