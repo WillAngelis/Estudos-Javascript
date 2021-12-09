d7de4cb1e66072adf7f2f109d054c762882cfa98
William Angelis <willangelis@gmail.com>
2021-12-09 18:44:10 -0300

    Arrumando erro de digitação

--- js/1- console-log.js
+++ js/1- console-log.js
@@ -2,4 +2,4 @@ console.log('WillAngelis');
 console.log("WillAngelis");
 console.log(`WillAngelis`); //Strings = Texto
 
-console.log(12345, 14.52)//Mumber = Não há separações de números flutuantes e inteiros todos são do tipo Number
+console.log(12345, 14.52)//Number = Não há separações de números flutuantes e inteiros todos são do tipo Number
