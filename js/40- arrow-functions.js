e62f168cb7dc1a9e3314bc0d7f02e0d4951e4cde
William Angelis <willangelis@gmail.com>
2021-12-08 22:07:11 -0300

    Estudo sobre arrow functions

---
+++ js/40- arrow-functions.js
@@ -0,0 +1,10 @@
+// let duplicar = function duplo(x = 3) { função declarativa
+//    return x * 2    
+// }
+
+let duplicar = (x = 3) => x * 2 // arrow function com retorno implicito
+console.log(duplicar(10))
+
+let mais = (x,y) => {
+    return x + y 
+} 
