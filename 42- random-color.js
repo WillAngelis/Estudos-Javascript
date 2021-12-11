fcaf6d47122c8f705220541b8de486167fce5685
William Angelis <willangelis@gmail.com>
2021-12-10 22:54:42 -0300

    Selecionando botões

--- 42- random-color.js
+++ 42- random-color.js
@@ -1,3 +1,8 @@
 const div = document.querySelector('.container')
 const p = document.querySelector('.texto')
-p.textContent = 'ola'
+const txt = document.querySelector('.txt')
+const cor = document.querySelector('.cor')
+
+let colors = ['red,blue,black,white,green,slateport']
+let texto = ['1,2,3,4,5,6,7,8,+4']
+let textoRandom = colors[Math.floor(Math.random()* colors.length)] 
