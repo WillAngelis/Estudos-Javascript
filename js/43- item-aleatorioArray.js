8c61392a605190c755853da1a45de4f2cf621613
William Angelis <willangelis@gmail.com>
2021-12-11 14:13:48 -0300

    Algoritmo para sortear item de Array

---
+++ js/43- item-aleatorioArray.js
@@ -0,0 +1,8 @@
+// Declarando o array
+const itens = ['casa','cachorro','gato','batata'];
+
+// Pegando item de forma aleatória
+const randomItem = itens[Math.floor(Math.random()*itens.length)]
+
+//Exibindo item
+console.log(randomItem);
