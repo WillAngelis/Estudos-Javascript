8e881c5d447c3741e53015f86c997b2e6f823ad4
William Angelis <willangelis@gmail.com>
2021-12-10 23:25:12 -0300

    Terminando lógica da randomização

--- 42- random-color.js
+++ 42- random-color.js
@@ -3,6 +3,23 @@ const p = document.querySelector('.texto')
 const txt = document.querySelector('.txt')
 const cor = document.querySelector('.cor')
 
-let colors = ['red,blue,black,white,green,slateport']
-let texto = ['1,2,3,4,5,6,7,8,+4']
-let textoRandom = colors[Math.floor(Math.random()* colors.length)] 
+let colors = ['red','blue','black','white','green','pink']
+let texto = ['1','2','3','4','5','6','7','8','+4']
+
+txt.addEventListener('click', function(){
+    let textoRandom = texto[Math.floor(Math.random() * texto.length)] 
+    p.textContent = textoRandom
+    console.log(textoRandom)
+
+})
+
+cor.addEventListener('click',()=>{ 
+    let corRandom = colors[Math.floor(Math.random() * colors.length)] 
+    div.style.background = corRandom
+    if (corRandom != 'white'){
+        p.style.color = 'white'
+    }else{
+        p.style.color = 'black'
+    }
+    console.log(corRandom)
+})
