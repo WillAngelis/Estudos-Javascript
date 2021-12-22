431fe6ff2407a6fc9250a31405a30f9aa2cc626e
William Angelis <willangelis@gmail.com>
2021-12-21 22:18:35 -0300

    Exercicio com if

---
+++ js/44- tipos-de-dados.js
@@ -0,0 +1,7 @@
+let nome = 'William'
+const idade = 25
+var isCoder = true
+
+console.log(nome)
+console.log(idade)
+console.log(isCoder)
---
+++ js/45- exercicios-if.js
@@ -0,0 +1,8 @@
+let idadePermitida = 18
+let idade = 22
+
+if (idade < idadePermitida){
+  console.log('Você não pode entrar')
+  }else{
+    console.log('ta liberado')
+  }
